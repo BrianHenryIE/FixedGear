1d7e9e594750dabc459395b3339e641ce6521ac0
Brian Henry <BrianHenryIE@gmail.com>
2016-06-13 23:50:28 +0100

    Initial Commit

---
+++ DublinBicycleStands/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  DublinBicycleStands
+//
+//  Created by Brian Henry on 13/06/2016.
+//  Copyright © 2016 Brian Henry. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ DublinBicycleStandsTests/DublinBicycleStandsTests.swift
@@ -0,0 +1,36 @@
+//
+//  DublinBicycleStandsTests.swift
+//  DublinBicycleStandsTests
+//
+//  Created by Brian Henry on 13/06/2016.
+//  Copyright © 2016 Brian Henry. All rights reserved.
+//
+
+import XCTest
+@testable import DublinBicycleStands
+
+class DublinBicycleStandsTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
